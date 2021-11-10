be6f0b7088632f666469f6c2f83b175dcd462acc
Jasem Duncan <jasemvalencia@gmail.com>
2021-11-10 10:51:58 -0500

    update test task 1

--- Task1.js
+++ Task1.js
@@ -1,10 +1,20 @@
 //1.- Character Count
+// function stringLength(string){
+//     let arr=[];
+//     arr=[...string];  
+//     console.log(arr.length);
+//     return arr.length;
+// }
+  
 function stringLength(string){
-    let arr=[];
-    arr=[...string];  
-    console.log(arr.length);
-    return arr.length;
+    let arr=[]
+    arr=[...string];
+    if(arr.length>0 && arr.length<11){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
   
-  
-  module.exports=stringLength;
+module.exports=stringLength;
--- Task1.test.js
+++ Task1.test.js
@@ -1,6 +1,15 @@
 //1.- Test Character Count
+// const stringLength = require('./Task1');
+
+// test('test characters count',()=>{
+//     expect(typeof stringLength('abcde')).toBe('number');
+//   })
+
+
 const stringLength = require('./Task1');
 
 test('test characters count',()=>{
-    expect(typeof stringLength('abcde')).toBe('number');
+    expect(stringLength('12345678910')).toBeFalsy();
   })
+  
+
